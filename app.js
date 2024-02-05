f22cf6de2ee6966f61398fd8a2fc1c9db38d0ba9
qkralsgml78 <qkralsgml78@gmail.com>
2024-02-05 15:08:00 +0900

    Router

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const router = express.Router();
+const port = 3000
+
+app.get('/', (req, res) => {
+	res.send('Hello World!')
+})
+
+    app.listen(port, () => {
+	console.log(`example app listening on port ${port}`)
+    })
+
+router.get('/artists/:id',function (req, res) {
+	console.log("id는 " +req.params.id +"입니다")
+	res.send("id: "+req.params.id)
+	});
+
+//여러개도 가능
+router.get('artists/:id/company', function(req, res) {
+	res.send("id : " +req.params.id +"회사: "+ req.params.company)
+	});
